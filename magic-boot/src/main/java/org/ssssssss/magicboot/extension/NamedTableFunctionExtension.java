5d577a99a6a1ac90aa6938f28199fae269f798b3
吕金泽 <1098696801@qq.com>
2021-12-29 17:50:08 +0800

    升级s8

--- magic-boot/src/main/java/org/ssssssss/magicboot/extension/NamedTableFunctionExtension.java
+++ magic-boot/src/main/java/org/ssssssss/magicboot/extension/NamedTableFunctionExtension.java
@@ -5,6 +5,7 @@ import org.ssssssss.magicapi.modules.table.NamedTable;
 import org.ssssssss.magicboot.model.MagicBootConstants;
 import org.ssssssss.script.annotation.Comment;
 import org.ssssssss.script.functions.ExtensionMethod;
+import org.ssssssss.script.runtime.RuntimeContext;
 
 import java.util.Map;
 
@@ -17,15 +18,15 @@ public class NamedTableFunctionExtension implements ExtensionMethod {
     }
 
     @Comment("保存到表中，当主键有值时则修改，否则插入")
-    public static Object saveOrUpdate(NamedTable namedTable, @Comment("各项列和值") Map<String, Object> data){
+    public static Object saveOrUpdate(NamedTable namedTable, RuntimeContext runtimeContext, @Comment("各项列和值") Map<String, Object> data){
         namedTable.setAttribute(MagicBootConstants.COMMON_FIELD, true);
-        namedTable.save(data,true);
+        namedTable.save(runtimeContext, data,true);
         return namedTable.getAttribute(MagicBootConstants.ID);
     }
 
     @Comment("保存到表中，当主键有值时则修改，否则插入")
-    public static Object saveOrUpdate(NamedTable namedTable){
-        return saveOrUpdate(namedTable, null);
+    public static Object saveOrUpdate(NamedTable namedTable, RuntimeContext runtimeContext){
+        return saveOrUpdate(namedTable, runtimeContext, null);
     }
 
 }
