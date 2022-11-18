de3fab92f84714b9975133d3f8ccbb1d811a3f14
吕金泽 <1098696801@qq.com>
2022-11-18 20:57:17 +0800

    优化按钮权限控制

--- magic-boot-ui/src/scripts/hasPermission.js
+++ magic-boot-ui/src/scripts/hasPermission.js
@@ -2,11 +2,11 @@
 const hasPermission = {
   install(app) {
     app.directive('permission', {
-      beforeMount(el, binding) {
+      mounted(el, binding) {
         if (binding.value) {
           const permissionList = app.config.globalProperties.$global.user.authorities
           if (permissionList && permissionList.length && !permissionList.includes(binding.value) && import.meta.env.MODE != 'demo') {
-            el.style.display = 'none'
+            el.remove()
           }
         }
       }
