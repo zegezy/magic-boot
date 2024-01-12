e469b0fda0e09a20cd4aec9f9cd90ad495104459
zegezy <1098696801@qq.com>
2024-01-12 10:54:23 +0800

    兼容接口多个"/"，比如//system/xxx

--- src/main/java/org/ssssssss/magicboot/configuration/WebConfiguration.java
+++ src/main/java/org/ssssssss/magicboot/configuration/WebConfiguration.java
@@ -1,8 +1,10 @@
 package org.ssssssss.magicboot.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
 import org.ssssssss.magicboot.model.Global;
 
 @Configuration
@@ -11,6 +13,12 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(Global.USER_FILES_BASE_URL + "**").addResourceLocations("file:" + Global.getDir() + Global.USER_FILES_BASE_URL);
+    }
+
+    // 兼容接口多个"/"，比如//system/xxx
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUrlPathHelper(new UrlPathHelper());
     }
 
 }
