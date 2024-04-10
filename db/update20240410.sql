663ae670cbd23082445194332643fcf1c775a4eb
zegezy <1098696801@qq.com>
2024-04-10 21:16:45 +0800

    更新数据库

---
+++ db/update20240410.sql
@@ -0,0 +1,3 @@
+ALTER TABLE sys_dynamic_component ADD type INT(11) comment "0：分组，1：vue组件" AFTER compile_css;
+ALTER TABLE sys_dynamic_component ADD remark text comment "备注" AFTER type;
+update sys_dynamic_component set type = 0 where id in(select pid from sys_dynamic_component where is_del = 0) and is_del = 0
